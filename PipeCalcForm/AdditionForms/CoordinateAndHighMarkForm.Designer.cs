bd286dcc427cf4fe08bfed901382e1945826da77
AlexBit <alviraExtra@outlook.com>
2021-11-15 22:56:13 +0300

    Creating tableOfX&Z

---
+++ PipeCalcForm/AdditionForms/CoordinateAndHighMarkForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace PipeCalcForm
+{
+    partial class CoordinateAndHighMarkForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridXandZ = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridXandZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridXandZ
+            // 
+            this.dataGridXandZ.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridXandZ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridXandZ.Location = new System.Drawing.Point(0, 0);
+            this.dataGridXandZ.Name = "dataGridXandZ";
+            this.dataGridXandZ.Size = new System.Drawing.Size(246, 274);
+            this.dataGridXandZ.TabIndex = 0;
+            // 
+            // CoordinateAndHighMarkForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(246, 274);
+            this.Controls.Add(this.dataGridXandZ);
+            this.Name = "CoordinateAndHighMarkForm";
+            this.Text = "CoordinateAndHighMarkTable";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridXandZ)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridXandZ;
+    }
+}
---
+++ PipeCalcForm/AdditionForms/CoordinateAndHighMarkForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PipeCalcForm
+{
+    public partial class CoordinateAndHighMarkForm : Form
+    {
+        public CoordinateAndHighMarkForm()
+        {
+            InitializeComponent();
+            IList<int> X_Spots = new List<int>(Enumerable.Range(0, 121)).Where(x => x % 5 == 0).ToList();
+            IList<int> Z_Spots = new List<int>();
+            for (int i = 0; i < X_Spots.Count; i++)
+            {
+                Z_Spots.Add(new Random().Next(35, 55));
+            }
+
+
+            var source = new BindingSource();
+
+            DataTable tab = new DataTable();
+            tab.Columns.Add("Координата, км", typeof(double));
+            tab.Columns.Add("Высотная отметка, м", typeof(double));
+            
+            for (int i = 0; i < X_Spots.Count; i++)
+            {
+                tab.Rows.Add(X_Spots[i], Z_Spots[i]);
+            }
+
+            dataGridXandZ.DataSource = tab;
+
+            tab.Rows.Add(200, 100);
+        }
+    }
+}
